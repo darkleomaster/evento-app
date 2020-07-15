c5af1e40db03c05499f07ab5522b5a55b7c3989a
darkleomaster <leo_masterwolf@hotmail.com>
2020-07-15 06:26:44 -0300

    Inclusão Controllers e FormEvento

--- src/main/java/EventoappApplication.java
+++ src/main/java/EventoappApplication.java
@@ -1,8 +1,10 @@
-package com.eventoapp.eventoapp;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan({"com.eventoapp.controllers"})
 @SpringBootApplication
 public class EventoappApplication {
 
