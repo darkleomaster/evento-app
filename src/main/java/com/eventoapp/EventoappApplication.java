96f0da9b8647e1bc15d27b29592f53e77f72db07
darkleomaster <leo_masterwolf@hotmail.com>
2020-07-17 05:18:36 -0300

    Efetuada Inserção em Banco de Dados MySQL

--- src/main/java/com/eventoapp/EventoappApplication.java
+++ src/main/java/com/eventoapp/EventoappApplication.java
@@ -3,10 +3,13 @@ package com.eventoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 @ComponentScan(basePackages={"com.eventoapp.controllers"})
+@EntityScan(basePackages={"com.eventoapp.models"})
 public class EventoappApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/eventoapp/models/Evento.java
+++ src/main/java/com/eventoapp/models/Evento.java
@@ -1,6 +1,20 @@
-package com.eventosapp.models;
+package com.eventoapp.models;
 
-public class Evento {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Evento implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	private long codigo;
 	
 	private String nome;
 	private String horario;
